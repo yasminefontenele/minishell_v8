b715f6c8a3398d5a431caf67e2541a3afe752717
emencova <eliskam@eliska.eliska>
2024-10-08 20:16:00 +0100

    splitting finished

--- src/exec/heredocs.c
+++ src/exec/heredocs.c
@@ -2,6 +2,94 @@
 
 #include "execute.h"
 
+int read_input_lines(char *input_buffer[2], size_t limit_length, char *end_marker, char *error_message)
+{
+    int line_count;
+
+    line_count = 0;
+    while (g_exit_status != 130 && (input_buffer[0] == NULL ||
+           ft_strncmp(input_buffer[0], end_marker, limit_length) != 0 || 
+           ft_strlen(end_marker) != limit_length))
+    {
+        input_buffer[0] = readline("> ");
+        if (input_buffer[0] == NULL)
+        {
+            printf("%s (wanted `%s`)\n", error_message, end_marker);
+            break;
+        }
+        if (ft_strcmp(input_buffer[0], "EOF") == 0)
+        {
+            free(input_buffer[0]);
+            return -1; // Indicate EOF
+        }
+        line_count++;
+    }
+    return (line_count); // Return the total line count
+}
+
+char *construct_here_document(char *input_buffer[2], size_t line_count)
+{
+    char *temp;
+
+    while (line_count > 0)
+    {
+        temp = input_buffer[1];
+        input_buffer[1] = ft_strjoin(temp, input_buffer[0]); // Join the new input
+        free(temp);
+        temp = ft_strjoin(input_buffer[1], "\n"); // Add newline
+        free(input_buffer[1]);
+        input_buffer[1] = temp;
+        free(input_buffer[0]); // Free the current input
+        input_buffer[0] = NULL;
+        line_count--; // Decrement line count
+    }
+
+    return (input_buffer[1]); // Return the constructed here-document
+}
+
+char *read_here_document(char *input_buffer[2], size_t limit_length, char *end_marker, char *error_message)
+{
+    int line_count;
+
+    line_count = read_input_lines(input_buffer, limit_length, end_marker, error_message);
+    if (line_count == -1) // Check if EOF was encountered
+        return NULL;
+
+    if (line_count > 0)
+        printf("Here-document contains %u lines.\n", line_count);
+
+    return (construct_here_document(input_buffer, line_count)); // Build the final document
+}
+
+int create_here_document_fd(char *input_buffer[2], char *delimiter[2])
+{
+    int fd[2];
+
+    g_exit_status = 0;
+    if (pipe(fd) == -1)
+    {
+        m_perror(PIPE_READ, NULL, 1);
+        return (-1);
+    }
+    input_buffer[1] = read_here_document(input_buffer, 0, delimiter[0], delimiter[1]);
+    if (input_buffer[1] == NULL || ft_strlen(input_buffer[1]) == 0)
+    {
+        close(fd[PIPE_READ]);
+        close(fd[PIPE_WRITE]);
+        return (-1);
+    }
+    write(fd[PIPE_WRITE], input_buffer[1], ft_strlen(input_buffer[1]));
+    free(input_buffer[1]);
+    close(fd[PIPE_WRITE]);
+    if (g_exit_status == 130)
+    {
+        close(fd[PIPE_READ]);
+        return (-1);
+    }
+    return (fd[PIPE_READ]);
+}
+
+/*
 char *read_here_document(char *input_buffer[2], size_t limit_length, char *end_marker, char *error_message)
 {
     char *temp;
@@ -37,3 +125,4 @@ char *read_here_document(char *input_buffer[2], size_t limit_length, char *end_m
         printf("Here-document contains %zu lines.\n", line_count); 
     return (input_buffer[1]);
 }
+*/
--- src/exec/process_files.c
+++ src/exec/process_files.c
@@ -6,41 +6,93 @@
 /*   By: eliskam <eliskam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:25:13 by emencova          #+#    #+#             */
-/*   Updated: 2024/10/08 20:06:47 by eliskam          ###   ########.fr       */
+/*   Updated: 2024/10/08 20:12:45 by eliskam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
 
-
-int create_here_document_fd(char *input_buffer[2], char *delimiter[2])
+int handle_output_redirection(t_exec *exec, char **args, int i, int append)
 {
-    int fd[2];
+    if (!args[i + 1])
+    {
+        printf("minishell: syntax error near unexpected token `newline'\n");
+        exec->out = -1;
+        return 0;
+    }
+    
+    exec->out = open_fd(exec->out, args[i + 1], 1, append);
+    if (exec->out == -1)
+    {
+        perror("Error opening output file");
+        return 0;
+    }
 
-    g_exit_status = 0;
-    if (pipe(fd) == -1)
-    {
-        m_perror(PIPE_READ, NULL, 1);
-        return (-1);
-    }
-    input_buffer[1] = read_here_document(input_buffer, 0, delimiter[0], delimiter[1]);
-    if (input_buffer[1] == NULL || ft_strlen(input_buffer[1]) == 0)
-    {
-        close(fd[PIPE_READ]);
-        close(fd[PIPE_WRITE]);
-        return (-1);
-    }
-    write(fd[PIPE_WRITE], input_buffer[1], ft_strlen(input_buffer[1]));
-    free(input_buffer[1]);
-    close(fd[PIPE_WRITE]);
-    if (g_exit_status == 130)
-    {
-        close(fd[PIPE_READ]);
-        return (-1);
-    }
-    return (fd[PIPE_READ]);
+    args[i] = NULL;
+    args[i + 1] = NULL;
+    return 1;
 }
 
+int handle_input_redirection(t_exec *exec, char **args, int *i)
+{
+    if (!args[*i + 1])
+    {
+        printf("minishell: syntax error near unexpected token `newline'\n");
+        exec->in = -1;
+        return 0;
+    }
+    
+    exec = infile_one(exec, args, i);
+    if (exec->in == -1)
+        return 0;
+
+    return 5;
+}
+
+int handle_here_document_redirection(t_exec *exec, char **args, int *i)
+{
+    if (!args[*i + 1])
+    {
+        printf("minishell: syntax error near unexpected token `newline'\n");
+        exec->in = -1;
+        return 0;
+    }
+    
+    infile_two(exec, args, i);
+    if (exec->in == -1)
+        return 0;
+
+    return 5;
+}
+
+int parse_redir(t_exec *exec, char **args)
+{
+    int i = 0;
+
+    while (args[i])
+    {
+        if (ft_strcmp(args[i], ">") == 0)
+        {
+            return handle_output_redirection(exec, args, i, 0); // No append
+        }
+        else if (ft_strcmp(args[i], ">>") == 0)
+        {
+            return handle_output_redirection(exec, args, i, 1); // Append
+        }
+        else if (ft_strcmp(args[i], "<") == 0)
+        {
+            return handle_input_redirection(exec, args, &i);
+        }
+        else if (ft_strcmp(args[i], "<<") == 0)
+        {
+            return handle_here_document_redirection(exec, args, &i);
+        }
+        i++;
+    }
+    return 0;
+}
+
+/*
 int parse_redir(t_exec *exec, char **args)
 {
     int i;
@@ -117,3 +169,4 @@ int parse_redir(t_exec *exec, char **args)
 
 
 
+*/
